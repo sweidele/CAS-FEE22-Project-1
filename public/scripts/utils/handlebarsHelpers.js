74f798a80ce3f982def1bd7d32263ce6c94f5aad
Stephan <s.weidele@gmail.com>
2022-06-24 16:56:39 +0200

    Add icon for importance

--- public/scripts/utils/handlebarsHelpers.js
+++ public/scripts/utils/handlebarsHelpers.js
@@ -4,3 +4,10 @@ Handlebars.registerHelper('formatDate', function (data) {
     };
     return new Date(data).toLocaleString('de-DE', options); //ES6
 });
+
+Handlebars.registerHelper('loop', function(n, block) {
+    var accum = '';
+    for(var i = 0; i < n; ++i)
+        accum += block.fn(i);
+    return accum;
+});
