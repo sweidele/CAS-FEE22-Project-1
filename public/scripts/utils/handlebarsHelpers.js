6de8c3faf121de340622fa62ae5fb5308dc9f16e
Stephan <s.weidele@gmail.com>
2022-06-25 23:55:56 +0200

    Add dueDate Format

--- public/scripts/utils/handlebarsHelpers.js
+++ public/scripts/utils/handlebarsHelpers.js
@@ -1,11 +1,28 @@
 /* eslint-disable no-undef */
-Handlebars.registerHelper("formatDate", (data) => {
-  const options = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  };
-  return new Date(data).toLocaleString("de-DE", options); // ES6
+Handlebars.registerHelper("formatDueDate", (data) => {
+  let message = "";
+  if (data === undefined) {
+    return message;
+  }
+
+  const dueDate = new Date(data);
+  const curreentDate = new Date();
+  const difference = dueDate.getTime() - curreentDate.getTime();
+  const differneceDays = Math.ceil(difference / (1000 * 3600 * 24));
+
+  if (differneceDays < -1) {
+    message = `${differneceDays} days ago`;
+  } else if (differneceDays === -1) {
+    message = "a day ago";
+  } else if (differneceDays === 0) {
+    message = "today";
+  } else if (differneceDays === 1) {
+    message = "in a day";
+  } else {
+    message = `in ${differneceDays} days`;
+  }
+
+  return message;
 });
 
 Handlebars.registerHelper("loop", (n, block) => {
